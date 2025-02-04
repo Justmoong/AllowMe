d58b5a3130730ec077f0438dce0bec4fe05d9d2c
Justmoong <andudyun0504@gmail.com>
2025-02-05 04:04:37 +0900

    fix plist

--- Allow Me Extension/Resources/background.js
+++ Allow Me Extension/Resources/background.js
@@ -4,3 +4,16 @@ browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.greeting === "hello")
         return Promise.resolve({ farewell: "goodbye" });
 });
+
+chrome.runtime.onInstalled.addListener(() => {
+    console.log("Anti-RightClick Extension Installed");
+});
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status === "complete") {
+        chrome.scripting.executeScript({
+            target: { tabId: tabId },
+            files: ["content.js"]
+        });
+    }
+});
