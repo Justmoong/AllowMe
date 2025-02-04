2863136a862fbaf6a2aea4db9916d60eea8c1b35
Justmoong <andudyun0504@gmail.com>
2025-02-05 03:25:57 +0900

    Initial Commit

---
+++ Allow Me/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Allow Me
+//
+//  Created by ymy on 2/5/25.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
