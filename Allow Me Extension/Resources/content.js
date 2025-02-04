e31407620b7dfb4097302ebb9805a07e3aaf8344
Justmoong <andudyun0504@gmail.com>
2025-02-05 03:34:36 +0900

    init

--- Allow Me Extension/Resources/content.js
+++ Allow Me Extension/Resources/content.js
@@ -1,7 +1,22 @@
-browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
-    console.log("Received response: ", response);
-});
-
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
-});
+// content.js
+(function() {
+    'use strict';
+    
+    document.addEventListener('contextmenu', function(event) {
+        event.stopPropagation();
+    }, true);
+    
+    document.addEventListener('mousedown', function(event) {
+        event.stopPropagation();
+    }, true);
+    
+    document.addEventListener('selectstart', function(event) {
+        event.stopPropagation();
+    }, true);
+    
+    if (document.body) {
+        document.body.oncontextmenu = null;
+        document.body.ondragstart = null;
+        document.body.onselectstart = null;
+    }
+})();
