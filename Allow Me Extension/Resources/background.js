36c09009408682a82d8e4a51173c1a94d683e993
Justmoong <andudyun0504@gmail.com>
2025-02-05 11:18:45 +0900

    Fix Some Preference

--- Allow Me Extension/Resources/background.js
+++ Allow Me Extension/Resources/background.js
@@ -1,19 +1,19 @@
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
-
-    if (request.greeting === "hello")
-        return Promise.resolve({ farewell: "goodbye" });
-});
-
-chrome.runtime.onInstalled.addListener(() => {
-    console.log("Anti-RightClick Extension Installed");
-});
-
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === "complete") {
-        chrome.scripting.executeScript({
-            target: { tabId: tabId },
-            files: ["content.js"]
-        });
-    }
-});
+//browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//    console.log("Received request: ", request);
+//
+//    if (request.greeting === "hello")
+//        return Promise.resolve({ farewell: "goodbye" });
+//});
+//
+//chrome.runtime.onInstalled.addListener(() => {
+//    console.log("Anti-RightClick Extension Installed");
+//});
+//
+//chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+//    if (changeInfo.status === "complete") {
+//        chrome.scripting.executeScript({
+//            target: { tabId: tabId },
+//            files: ["content.js"]
+//        });
+//    }
+//});
